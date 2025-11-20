89bef4faec4c9b4af88475aacbbfb1b9f7e7be1a
abeltechtree-sketch <abeltechtree>
2025-11-20 12:57:01 +0300

    summissiion

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('hi');
+const smalls=document.querySelectorAll('.small');
+const inputText=document.querySelector('.input');
+
+
+inputText.addEventListener('input',function(){
+    const toBeFiltered = inputText.value.toLowerCase();
+    smalls.forEach(function(small){
+
+        const name_of_dataset =  small.dataset.name.toLowerCase();
+        console.log(name_of_dataset)
+
+        if(name_of_dataset.includes(toBeFiltered) || toBeFiltered === '') {
+            small.style.display ="inline-block"
+        }  else {
+            small.style.display ="none";
+        }
+        
+    })
+})
+
+
+
